f0f79b25ae780990b988759086ae29d48fbd90ff
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-23 11:56:48 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+
+module.exports = [
+  {
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".ts", ".tsx"],
+    },
+    plugins: [
+      new HTMLPlugin({
+        template: path.join(__dirname, "src/index.html"),
+      }),
+    ],
+  },
+]
