db3a5209fb2c9ffa5f6a2ba2f91b71230162c6c7
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-24 15:35:22 +0900

    get spotify access token

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,24 @@
 import React from "react"
 import { Button } from "@material-ui/core"
-import { Link } from "react-router-dom"
 
 export const Login = () => {
+  const redirectUri = encodeURIComponent(
+    `http://localhost:8080/spotify-callback`
+  )
+  const scope = encodeURIComponent(
+    "user-top-read user-read-private user-read-email"
+  )
+  const clientId = "ef635fbff37e468db46dee87459d7537"
+
   return (
     <>
-      <Link to="/">
-        <Button>Spoitfyで登録</Button>
-      </Link>
+      <Button
+        onClick={() => {
+          window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`
+        }}
+      >
+        Spoitfyで登録
+      </Button>
     </>
   )
 }
