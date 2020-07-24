5fb855b54e091159925f856b91137264ea8f37d4
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-24 09:53:50 +0900

    add appbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { fake } from "./fake"
+import { AppBar, Toolbar, Typography } from "@material-ui/core"
+import "./index.css"
 
 // https://musicle-server.herokuapp.com/
 
 const App = () => {
   return (
     <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Musicle</Typography>
+        </Toolbar>
+      </AppBar>
       <h2>あなたがSpotifyでよく聴く曲ランキング</h2>
       <ol>
         {fake.map((item) => (
