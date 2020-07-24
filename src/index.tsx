a5f25ecaa1c160ccf68e44848f4aba61a82a4948
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-24 09:32:51 +0900

    show song name and artist name

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { fake } from "./fake"
 
-ReactDOM.render(<p>すぐに変更できる</p>, document.getElementById("root"))
+// https://musicle-server.herokuapp.com/
+
+const App = () => {
+  return (
+    <>
+      <h2>あなたがSpotifyでよく聴く曲ランキング</h2>
+      <ol>
+        {fake.map((item) => (
+          <li>
+            {item.song_name} - {item.artist_name}
+          </li>
+        ))}
+      </ol>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
